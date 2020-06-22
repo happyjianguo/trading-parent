27c0edd8947d00ce924f6fb313de0e22f5f27144
jinxu <www.191936793@qq.com>
2020-06-22 18:29:14 +0800

    解决冲突

--- trading/src/main/java/com/dili/trading/controller/TransitionDepartureApplyController.java
+++ trading/src/main/java/com/dili/trading/controller/TransitionDepartureApplyController.java
@@ -15,6 +15,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+/**
+ * TransitionDepartureApplyController
+ */
 @Controller
 @RequestMapping("/transitionDepartureApplyController")
 public class TransitionDepartureApplyController {
