bdfb52cf49e0a2f709337895e6a00bd25631b51b
jinxu <www.191936793@qq.com>
2020-07-17 11:19:27 +0800

    修改页面，以及接口，完成支付

--- trading/src/main/java/com/dili/trading/controller/TransitionDepartureSettlementController.java
+++ trading/src/main/java/com/dili/trading/controller/TransitionDepartureSettlementController.java
@@ -26,6 +26,7 @@ import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 /**
  * 结算单接口
@@ -211,6 +212,9 @@ public class TransitionDepartureSettlementController {
     @RequestMapping(value = "/fee.action", method = {RequestMethod.GET, RequestMethod.POST})
     @ResponseBody
     public BaseOutput getFee(BigDecimal netWeight, Long id) {
+        if (Objects.isNull(netWeight)) {
+            return BaseOutput.failure("重量不能为空");
+        }
         UserTicket userTicket = SessionContext.getSessionContext().getUserTicket();
         return transitionDepartureSettlementRpc.getFee(netWeight, userTicket.getFirmId(), userTicket.getDepartmentId(), id);
     }
