9be78059b725f3e17d7e4e1e1a7e5a2d9dc35566
jinxu <www.191936793@qq.com>
2020-08-20 17:29:22 +0800

    mq配置

---
+++ trading/src/main/java/com/dili/trading/config/GenericGlobalExceptionResolver.java
@@ -0,0 +1,42 @@
+package com.dili.trading.config;
+
+import com.dili.ss.domain.BaseOutput;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.net.SocketTimeoutException;
+
+/**
+ * @author:
+ * @date: 2020/4/8 11:22
+ */
+@ControllerAdvice
+public class GenericGlobalExceptionResolver {
+    @Autowired
+    private HttpServletRequest request;
+
+    @Autowired
+    private HttpServletResponse response;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(GenericGlobalExceptionResolver.class);
+
+    /**
+     * 连接超时捕获
+     *
+     * @author
+     * @date 2020/7/10
+     */
+    @ExceptionHandler({SocketTimeoutException.class})
+    @ResponseBody
+    public BaseOutput timeOut(SocketTimeoutException e) {
+        return BaseOutput.failure("连接超时");
+    }
+
+
+}
