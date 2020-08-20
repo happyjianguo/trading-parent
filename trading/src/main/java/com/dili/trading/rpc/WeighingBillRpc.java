b0f3021b80acbcd310d50f3d219ac123dcd357f5
jinxu <www.191936793@qq.com>
2020-08-20 15:49:00 +0800

    交易过磅rpc修改

--- trading/src/main/java/com/dili/trading/rpc/WeighingBillRpc.java
+++ trading/src/main/java/com/dili/trading/rpc/WeighingBillRpc.java
@@ -12,43 +12,43 @@ import com.dili.orders.domain.WeighingBill;
 import com.dili.orders.dto.WeighingBillDetailDto;
 import com.dili.orders.dto.WeighingBillListPageDto;
 import com.dili.orders.dto.WeighingBillQueryDto;
+import com.dili.orders.dto.WeighingBillUpdateDto;
 import com.dili.ss.domain.BaseOutput;
 import com.dili.ss.domain.PageOutput;
 
 @FeignClient(name = "order-service", contextId = "weighingBill")
 public interface WeighingBillRpc {
 
-	@RequestMapping(value = "/api/weighingBill/insert")
-	BaseOutput<String> add(@RequestBody WeighingBill weighingBill);
+    @RequestMapping(value = "/api/weighingBill/insert")
+    BaseOutput<String> add(@RequestBody WeighingBill weighingBill);
 
-	@RequestMapping(value = "/api/weighingBill/update")
-	BaseOutput<Object> update(@RequestBody WeighingBill weighingBill);
+    @RequestMapping(value = "/api/weighingBill/update")
+    BaseOutput<Object> update(@RequestBody WeighingBill weighingBill);
 
-	@RequestMapping(value = "/api/weighingBill/settle")
-	BaseOutput<Object> settle(@RequestParam String serialNo, @RequestParam String buyerPassword, @RequestParam Long operatorId);
+    @RequestMapping(value = "/api/weighingBill/settle")
+    BaseOutput<Object> settle(@RequestParam(value = "serialNo") String serialNo, @RequestParam(value = "buyerPassword") String buyerPassword, @RequestParam(value = "operatorId") Long operatorId, @RequestParam(value = "marketId") Long marketId);
 
-	@RequestMapping(value = "/api/weighingBill/withdraw")
-	BaseOutput<Object> withdraw(@RequestParam String serialNo, @RequestParam String buyerPassword, @RequestParam String sellerPassword, @RequestParam Long operatorId);
+    @RequestMapping(value = "/api/weighingBill/withdraw")
+    BaseOutput<Object> withdraw(@RequestParam(value = "serialNo") String serialNo, @RequestParam(value = "buyerPassword") String buyerPassword, @RequestParam(value = "sellerPassword") String sellerPassword, @RequestParam(value = "operatorId") Long operatorId);
 
-	@RequestMapping(value = "/api/weighingBill/invalidate")
-	BaseOutput<Object> invalidate(@RequestParam String serialNo, @RequestParam String buyerPassword, @RequestParam String sellerPassword, @RequestParam Long operatorId);
+    @RequestMapping(value = "/api/weighingBill/invalidate")
+    BaseOutput<Object> invalidate(@RequestParam(value = "serialNo") String serialNo, @RequestParam(value = "buyerPassword") String buyerPassword, @RequestParam(value = "sellerPassword") String sellerPassword, @RequestParam(value = "operatorId") Long operatorId);
 
-	@RequestMapping(value = "/api/weighingBill/close")
-	BaseOutput<Object> close(@RequestParam String serialNo);
+    @RequestMapping(value = "/api/weighingBill/close")
+    BaseOutput<Object> close(@RequestParam(value = "serialNo") String serialNo);
 
-	@RequestMapping(value = "/api/weighingBill/listByExample", method = RequestMethod.POST)
-	BaseOutput<Object> listByExample(@RequestBody WeighingBillQueryDto queryDto);
+    @RequestMapping(value = "/api/weighingBill/listByExample", method = RequestMethod.POST)
+    BaseOutput<Object> listByExample(@RequestBody WeighingBillQueryDto queryDto);
 
-	@RequestMapping(value = "/api/weighingBill/listPage", method = RequestMethod.POST)
-	PageOutput<List<WeighingBillListPageDto>> listPage(@RequestBody WeighingBillQueryDto query);
+    @RequestMapping(value = "/api/weighingBill/listPage", method = RequestMethod.POST)
+    PageOutput<List<WeighingBillListPageDto>> listPage(@RequestBody WeighingBillQueryDto query);
 
-	@RequestMapping(value = "/api/weighingBill/detail")
-	BaseOutput<WeighingBillDetailDto> findDetailDtoById(@RequestParam Long id);
+    @RequestMapping(value = "/api/weighingBill/detail")
+    BaseOutput<WeighingBillDetailDto> findDetailDtoById(@RequestParam(value = "id") Long id);
 
-	@RequestMapping(value = "/api/weighingBill/operatorInvalidate")
-	BaseOutput<Object> operatorInvalidate(@RequestParam Long id, @RequestParam Long operatorId, @RequestParam String operatorPassword);
+    @RequestMapping(value = "/api/weighingBill/operatorInvalidate")
+    BaseOutput<Object> operatorInvalidate(@RequestParam(value = "id") Long id, @RequestParam(value = "operatorId") Long operatorId, @RequestParam(value = "operatorPassword") String operatorPassword);
 
-	@RequestMapping(value = "/api/weighingBill/operatorWithdraw")
-	BaseOutput<Object> operatorWithdraw(@RequestParam Long id, @RequestParam Long operatorId, @RequestParam String operatorPassword);
-
+    @RequestMapping(value = "/api/weighingBill/operatorWithdraw")
+    BaseOutput<Object> operatorWithdraw(@RequestParam(value = "id") Long id, @RequestParam(value = "operatorId") Long operatorId, @RequestParam(value = "operatorPassword") String operatorPassword);
 }
