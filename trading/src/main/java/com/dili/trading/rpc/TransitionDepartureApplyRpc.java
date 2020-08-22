963caa6e2bce1030614be58632f2007e394ad104
jinxu <www.191936793@qq.com>
2020-08-22 17:01:12 +0800

    修改rpc，不需要url，并且注册服务

--- trading/src/main/java/com/dili/trading/rpc/TransitionDepartureApplyRpc.java
+++ trading/src/main/java/com/dili/trading/rpc/TransitionDepartureApplyRpc.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@FeignClient(name = "order-service", contextId = "transitionDepartureApplyRpc", url = "localhost:8185")
+@FeignClient(name = "order-service", contextId = "transitionDepartureApplyRpc")
 public interface TransitionDepartureApplyRpc {
 
     /**
