c8889d376210a5849cfeeaaa8007e43cf25db96f
Seabert.Zhan <seabert.zhan@skywares.com>
2020-09-07 16:54:58 +0800

    获取参考价

--- trading/src/main/java/com/dili/trading/controller/RefrencePriceController.java
+++ trading/src/main/java/com/dili/trading/controller/RefrencePriceController.java
@@ -1,27 +1,9 @@
 package com.dili.trading.controller;
 
-import com.alibaba.fastjson.JSONObject;
-import com.dili.assets.sdk.dto.CategoryDTO;
-import com.dili.assets.sdk.rpc.CategoryRpc;
-import com.dili.orders.domain.ComprehensiveFee;
-import com.dili.orders.rpc.CardRpc;
 import com.dili.ss.domain.BaseOutput;
-import com.dili.ss.domain.EasyuiPageOutput;
-import com.dili.ss.domain.PageOutput;
-import com.dili.ss.metadata.ValueProvider;
-import com.dili.ss.metadata.ValueProviderUtils;
-import com.dili.trading.rpc.ComprehensiveFeeRpc;
 import com.dili.trading.rpc.ReferencePriceRpc;
-import com.dili.trading.service.ComprehensiveFeeService;
-import com.dili.uap.sdk.domain.UserTicket;
-import com.dili.uap.sdk.glossary.DataAuthType;
-import com.dili.uap.sdk.session.SessionContext;
-import com.google.common.collect.Lists;
-import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
--- trading/src/main/java/com/dili/trading/rpc/ReferencePriceRpc.java
+++ trading/src/main/java/com/dili/trading/rpc/ReferencePriceRpc.java
@@ -1,8 +1,6 @@
 package com.dili.trading.rpc;
 
-import com.dili.orders.domain.ComprehensiveFee;
 import com.dili.ss.domain.BaseOutput;
-import com.dili.ss.domain.PageOutput;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 
