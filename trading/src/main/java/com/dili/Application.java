62ea62dae6746797e47012430bc99f5b4367cf79
jiang <jiang@DESKTOP-GJ4LO3T>
2020-09-17 14:51:54 +0800

    配置类未提交

--- trading/src/main/java/com/dili/Application.java
+++ trading/src/main/java/com/dili/Application.java
@@ -9,24 +9,32 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.dili.ss.retrofitful.annotation.RestfulScan;
+import com.dili.trading.config.PutMchIdHeaderInterceptor;
 
 /**
  * 由MyBatis Generator工具自动生成
  */
 @SpringBootApplication
-@ComponentScan(basePackages = { "com.dili.ss", "com.dili.trading", "com.dili.uap.sdk", "com.dili.logger.sdk" })
+@ComponentScan(basePackages = { "com.dili.ss", "com.dili.trading", "com.dili.orders.config", "com.dili.uap.sdk", "com.dili.logger.sdk" })
 @RestfulScan({ "com.dili.trading.rpc", "com.dili.uap.sdk.rpc", "com.dili.bpmc.sdk.rpc" })
 //@DTOScan(value = {"com.dili.ss"})
 @EnableDiscoveryClient
 @EnableFeignClients
-public class Application extends SpringBootServletInitializer {
+public class Application extends SpringBootServletInitializer implements WebMvcConfigurer {
 
 	@LoadBalanced
 	@Bean
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new PutMchIdHeaderInterceptor());
 	}
 
 	public static void main(String[] args) {
