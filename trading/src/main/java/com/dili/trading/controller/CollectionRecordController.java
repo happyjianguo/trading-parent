c4be903724aa3f5f92aa97f2caa18664576b5c64
jinxu <www.191936793@qq.com>
2021-01-27 18:14:05 +0800

    回款单据页面完成部分

--- trading/src/main/java/com/dili/trading/controller/CollectionRecordController.java
+++ trading/src/main/java/com/dili/trading/controller/CollectionRecordController.java
@@ -1,6 +1,7 @@
 package com.dili.trading.controller;
 
 import com.dili.orders.domain.CollectionRecord;
+import com.dili.ss.domain.BaseOutput;
 import com.dili.ss.domain.EasyuiPageOutput;
 import com.dili.ss.domain.PageOutput;
 import com.dili.ss.metadata.ValueProviderUtils;
@@ -17,6 +18,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 /**
@@ -34,8 +37,17 @@ public class CollectionRecordController {
      * 跳转页面
      */
     @RequestMapping(value = "/list.html", method = RequestMethod.GET)
-    public String index(ModelMap modelMap) {
+    public String list(ModelMap modelMap) {
         return "collectionRecord/list";
+    }
+
+
+    /**
+     * 跳转页面
+     */
+    @RequestMapping(value = "/groupList.html", method = RequestMethod.GET)
+    public String groupList(ModelMap modelMap) {
+        return "collectionRecord/groupList";
     }
 
     /**
@@ -58,5 +70,38 @@ public class CollectionRecordController {
         }
         PageOutput<List<CollectionRecord>> output = collectionRecordRpc.listByQueryParams(collectionRecord);
         return new EasyuiPageOutput(output.getTotal(), ValueProviderUtils.buildDataByProvider(collectionRecord, output.getData())).toString();
+    }
+
+    /**
+     * 根据参数查询数据
+     *
+     * @param collectionRecord
+     * @return
+     * @throws Exception
+     */
+    @RequestMapping(value = "/groupListByQueryParams.action", method = {RequestMethod.GET, RequestMethod.POST})
+    @ResponseBody
+    public String groupListByQueryParams(@RequestBody CollectionRecord collectionRecord) throws Exception {
+        //判断买家id或买家卡账户是否为空，必须存在一个
+        if (Objects.isNull(collectionRecord.getBuyerId()) && Objects.isNull(collectionRecord.getAccountBuyerId())) {
+            return "买家id或买家卡账户不能为空";
+        }
+        //判断卖家id和卖家卡账户是否为空，必须存在一个
+        if (Objects.isNull(collectionRecord.getSellerId()) && Objects.isNull(collectionRecord.getAccountSellerId())) {
+            return "卖家id或卖家卡账户不能为空";
+        }
+        //判断查询的部门权限id是否为空，如果为空则查询所有的，如果不为空则查询单独的
+        List<Long> departments = SessionContext.getSessionContext().dataAuth(DataAuthType.DEPARTMENT.getCode()).stream().map(x -> Long.valueOf((String) x.get("value"))).collect(Collectors.toList());
+        //数据权限，根据部门查询
+        if (CollectionUtils.isNotEmpty(departments)) {
+            collectionRecord.setDepartmentIds(departments);
+        }
+        //设置市场id
+        collectionRecord.setMarketId(SessionContext.getSessionContext().getUserTicket().getFirmId());
+        BaseOutput<List<Map<String, String>>> baseOutput = collectionRecordRpc.groupListForDetail(collectionRecord);
+        if (!baseOutput.isSuccess()) {
+            return "查询失败";
+        }
+        return new EasyuiPageOutput(Long.valueOf(baseOutput.getData().size()), baseOutput.getData()).toString();
     }
 }
--- trading/src/main/java/com/dili/trading/rpc/CollectionRecordRpc.java
+++ trading/src/main/java/com/dili/trading/rpc/CollectionRecordRpc.java
@@ -1,18 +1,21 @@
 package com.dili.trading.rpc;
 
 import com.dili.orders.domain.CollectionRecord;
+import com.dili.ss.domain.BaseOutput;
 import com.dili.ss.domain.PageOutput;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * 回款记录相关rpc
  */
-@FeignClient(name = "order-service", contextId = "collectionRecordRpc", url = "${orderService.url:}")
+@FeignClient(name = "order-service", contextId = "collectionRecordRpc", url = "localhost:8185")
 public interface CollectionRecordRpc {
 
     /**
@@ -23,4 +26,8 @@ public interface CollectionRecordRpc {
      */
     @RequestMapping(value = "/api/collectionRecord/listByQueryParams", method = {RequestMethod.POST})
     PageOutput<List<CollectionRecord>> listByQueryParams(@RequestBody CollectionRecord collectionRecord);
+
+
+    @PostMapping("/api/collectionRecord/groupListForDetail")
+    BaseOutput<List<Map<String, String>>> groupListForDetail(@RequestBody CollectionRecord collectionRecord);
 }
