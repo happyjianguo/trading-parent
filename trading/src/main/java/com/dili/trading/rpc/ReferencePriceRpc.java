ac2fc8581a33393f1335968119f9bfc84f60d465
miaoguoxin <398544597@qq.com>
2021-02-08 10:47:15 +0800

    中间价获取

--- trading/src/main/java/com/dili/trading/rpc/ReferencePriceRpc.java
+++ trading/src/main/java/com/dili/trading/rpc/ReferencePriceRpc.java
@@ -2,16 +2,18 @@ package com.dili.trading.rpc;
 
 import com.dili.ss.domain.BaseOutput;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * 获取参考价RPC
  *
- *@author  Henry.Huang
- *@date  2020/08/20
+ *@author Henry.Huang
+ *@date 2020/08/20
  *
  */
-@FeignClient(name = "order-service", contextId = "referencePriceRpc",url="${orderService.url:}")
+@FeignClient(name = "order-service", contextId = "referencePriceRpc", url = "${orderService.url:}")
 public interface ReferencePriceRpc {
 
 
@@ -23,7 +25,7 @@ public interface ReferencePriceRpc {
      * @return
      */
     @RequestMapping(value = "/api/refrencePrice/getReferencePriceByGoodsId", method = {RequestMethod.POST})
-    BaseOutput<Object> getReferencePriceByGoodsId(@RequestParam(value = "goodsId") Long goodsId, @RequestParam(value = "marketId") Long marketId, @RequestParam(value = "tradeType") String tradeType);
+    BaseOutput<Object> getReferencePriceByGoodsId(@RequestParam(value = "goodsId") Long goodsId, @RequestParam(value = "marketId") Long marketId, @RequestParam(value = "tradeType") String tradeType, @RequestParam(value = "type") Integer type);
 
 
 }
