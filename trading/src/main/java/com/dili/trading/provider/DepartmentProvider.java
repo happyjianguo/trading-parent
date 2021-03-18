08fe64f3b5b02d78432dbdca86b83f35a68def3a
jiang <jiang@DESKTOP-GJ4LO3T>
2021-03-18 17:27:24 +0800

    解决部门查询条件下拉框部门权限控制不正确的问题

--- trading/src/main/java/com/dili/trading/provider/DepartmentProvider.java
+++ trading/src/main/java/com/dili/trading/provider/DepartmentProvider.java
@@ -1,58 +1,69 @@
 package com.dili.trading.provider;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import com.dili.ss.domain.BaseOutput;
+import com.dili.ss.dto.DTOUtils;
 import com.dili.ss.metadata.BatchProviderMeta;
 import com.dili.ss.metadata.FieldMeta;
 import com.dili.ss.metadata.ValuePair;
 import com.dili.ss.metadata.ValuePairImpl;
 import com.dili.ss.metadata.provider.BatchDisplayTextProviderSupport;
 import com.dili.uap.sdk.domain.Department;
-import com.dili.uap.sdk.domain.UserTicket;
+import com.dili.uap.sdk.domain.dto.DepartmentDto;
 import com.dili.uap.sdk.glossary.DataAuthType;
 import com.dili.uap.sdk.rpc.DepartmentRpc;
 import com.dili.uap.sdk.session.SessionContext;
-import org.apache.commons.collections.CollectionUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Component;
-
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 /**
  * 权限部门
  */
 @Component
-@Scope("prototype")
 public class DepartmentProvider extends BatchDisplayTextProviderSupport {
 
-    @Autowired
-    private DepartmentRpc departmentRpc;
+	private static final Logger LOGGER = LoggerFactory.getLogger(DepartmentProvider.class);
 
-    @Override
-    public List<ValuePair<?>> getLookupList(Object obj, Map metaMap, FieldMeta fieldMeta) {
-        UserTicket userTicket = SessionContext.getSessionContext().getUserTicket();
-        BaseOutput<List<Department>> listBaseOutput = departmentRpc.listUserAuthDepartmentByFirmId(userTicket.getId(), userTicket.getFirmId());
-        if (!listBaseOutput.isSuccess() || CollectionUtils.isEmpty(listBaseOutput.getData())) {
-            return null;
-        }
-        List<ValuePair<?>> resultList = listBaseOutput.getData().stream().map(f -> {
-            return (ValuePair<?>) new ValuePairImpl(f.getName(), f.getId());
-        }).collect(Collectors.toList());
-        return resultList;
-    }
+	@Autowired
+	private DepartmentRpc departmentRpc;
 
+	@Override
+	public List<ValuePair<?>> getLookupList(Object obj, Map metaMap, FieldMeta fieldMeta) {
+		List<Map> deptDataAuths = SessionContext.getSessionContext().dataAuth(DataAuthType.DEPARTMENT.getCode());
+		if (CollectionUtils.isEmpty(deptDataAuths)) {
+			return null;
+		}
+		List<String> departmentIds = new ArrayList<String>(deptDataAuths.size());
+		deptDataAuths.forEach(da -> departmentIds.add(da.get("value").toString()));
+		DepartmentDto query = DTOUtils.newInstance(DepartmentDto.class);
+		query.setIds(departmentIds);
+		BaseOutput<List<Department>> listBaseOutput = this.departmentRpc.listByExample(query);
+		if (!listBaseOutput.isSuccess()) {
+			LOGGER.error(listBaseOutput.getMessage());
+			return null;
+		}
+		List<ValuePair<?>> resultList = listBaseOutput.getData().stream().map(f -> {
+			return (ValuePair<?>) new ValuePairImpl(f.getName(), f.getId());
+		}).collect(Collectors.toList());
+		return resultList;
+	}
 
-    @Override
-    protected BatchProviderMeta getBatchProviderMeta(Map metaMap) {
-        return null;
-    }
+	@Override
+	protected BatchProviderMeta getBatchProviderMeta(Map metaMap) {
+		return null;
+	}
 
-    @Override
-    protected List getFkList(List<String> relationIds, Map metaMap) {
-        return null;
-    }
-
+	@Override
+	protected List getFkList(List<String> relationIds, Map metaMap) {
+		return null;
+	}
 
 }
