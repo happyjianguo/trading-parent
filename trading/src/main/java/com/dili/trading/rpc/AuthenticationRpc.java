bca74c74da92e67369b38eed008e58ced842a3aa
jiang <jianglinhu@diligrp.com>
2021-04-02 15:27:52 +0800

    解决接入uap3.0报错的问题

--- trading/src/main/java/com/dili/trading/rpc/AuthenticationRpc.java
+++ trading/src/main/java/com/dili/trading/rpc/AuthenticationRpc.java
@@ -1,16 +1,14 @@
 package com.dili.trading.rpc;
 
-import java.util.Map;
-
 import com.dili.ss.domain.BaseOutput;
 import com.dili.ss.retrofitful.annotation.POST;
+import com.dili.ss.retrofitful.annotation.ReqParam;
 import com.dili.ss.retrofitful.annotation.Restful;
-import com.dili.ss.retrofitful.annotation.VOBody;
 
 @Restful("${uap.contextPath}")
 public interface AuthenticationRpc {
 
-	@POST("/authenticationApi/loginout.api")
-	BaseOutput<Object> loginout(@VOBody Map<String, String> sessionIdMap);
+	@POST("/authenticationApi/logout")
+	BaseOutput<Object> logout(@ReqParam("refreshToken") String refreshToken);
 
 }
